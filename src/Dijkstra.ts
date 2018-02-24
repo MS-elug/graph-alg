aa71b857df872c24aa65bbe350d6a20c89c8f80b
MS <mathieu.seiler@gmail.com>
2018-02-24 11:31:03 +0100

    Added lint configuration Added IDE format configuration

--- src/Dijkstra.ts
+++ src/Dijkstra.ts
@@ -1,5 +1,3 @@
-
-
 /**
  * Graph representation
  * @export
@@ -11,11 +9,11 @@ export interface IGraph {
      */
     [node: string]: {
         [node: string]: number;
-    }
+    };
 }
 
 /**
- * 
+ *
  * @interface IResult
  */
 interface IResult {
@@ -23,32 +21,32 @@ interface IResult {
     path: string[];
 }
 
-/** 
+/**
  * Implementation of the Dijkstra algorithm.
- * Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph.
- * 
+ * Dijkstra"s algorithm is an algorithm for finding the shortest paths between nodes in a graph.
+ *
  * @export
  * @class Dijkstra
  * @ref https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
- * 
+ *
  */
 export default class Dijkstra {
 
     constructor(private readonly graph: IGraph) {
         if (!graph) {
-            throw new Error('Class cannot instantiate without graph data');
+            throw new Error("Class cannot instantiate without graph data");
         }
         if (Object.keys(this.graph).length === 0) {
-            throw new Error('No graph data available');
+            throw new Error("No graph data available");
         }
     }
 
     /**
      * Resolve the algorithm from a sourceNode to a targetNode
-     * 
-     * @param {string} sourceNode 
-     * @param {string} targetNode 
-     * @returns {IResult} 
+     *
+     * @param {string} sourceNode
+     * @param {string} targetNode
+     * @returns {IResult}
      * @memberof Dijkstra
      */
     public resolve(sourceNode: string, targetNode: string): IResult {
@@ -62,8 +60,8 @@ export default class Dijkstra {
 
         const result = {
             distance: Number.POSITIVE_INFINITY,
-            path: []
-        }
+            path: [],
+        };
 
         // ----------------------------
         // #1 INIT
@@ -74,41 +72,44 @@ export default class Dijkstra {
         const P = [];
 
         // Create a structure to store computed cost from the source point to other nodes
-        const distance : {[node: string] : number } = {};
+        const distance: { [node: string]: number } = {};
         // Create a structure to store every node predecessor
-        const predecessor : {[node: string] : string }= {};
+        const predecessor: { [node: string]: string } = {};
         Q.forEach((node) => {
             distance[node] = node === sourceNode ? 0 : Number.POSITIVE_INFINITY;
             predecessor[node] = null;
         });
 
         // ----------------------------
-        // #2 
+        // #2
         // Init algo from source node
-        let node : string = sourceNode;
-        while (node) {
+        let currentNode: string = sourceNode;
+        while (currentNode) {
             // Get current distance from sourceNode
-            let distanceFromSource = distance[node];
+            const distanceFromSource = distance[currentNode];
             // Get current node children
-            let childrenDistanceFromCurrentNode = this.graph[node];
+            const childrenDistanceFromCurrentNode = this.graph[currentNode];
             // For every child node, save only the minimal distance
-            for (let n in childrenDistanceFromCurrentNode) {
-                let childDistanceFromSource = distanceFromSource + childrenDistanceFromCurrentNode[n];
+            for (const n in childrenDistanceFromCurrentNode) {
+                if (!childrenDistanceFromCurrentNode.hasOwnProperty(n)) {
+                    continue;
+                }
+                const childDistanceFromSource = distanceFromSource + childrenDistanceFromCurrentNode[n];
                 if (distance[n] > childDistanceFromSource) {
                     distance[n] = childDistanceFromSource;
-                    predecessor[n] = node;
+                    predecessor[n] = currentNode;
                 }
             }
             // Remove node from Q set and put it to P
-            P.push(Q.splice(Q.indexOf(node), 1)[0]);
-            node = this.lowestDistanceNode(Q, distance);
+            P.push(Q.splice(Q.indexOf(currentNode), 1)[0]);
+            currentNode = this.lowestDistanceNode(Q, distance);
         }
-        //Output the distance to reach the target nod from the source node
+        // Output the distance to reach the target nod from the source node
         result.distance = distance[targetNode];
 
         // ----------------------------
         // #3 Compute shortest path between source and target nodes
-        let optimalPath = [targetNode];
+        const optimalPath = [targetNode];
         let parent = predecessor[targetNode];
         while (parent) {
             optimalPath.push(parent);
@@ -123,7 +124,7 @@ export default class Dijkstra {
     /**
      * Compute the lowest distance from the remaing nodes in Q set
      */
-    private lowestDistanceNode = (Q: string[], distance : {[node: string] : number } ) => {
+    private lowestDistanceNode = (Q: string[], distance: { [node: string]: number }) => {
         return Q.reduce((lowestNode, node) => {
             if (lowestNode === null || distance[node] < distance[lowestNode]) {
                 lowestNode = node;
